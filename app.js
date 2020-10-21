5ea05014b6bd1dfbe65bd4af890b8d8480b04e28
Jimi Ahlgren <jimi.ahlgren@gmail.com>
2020-10-21 15:39:28 +0300

    init

---
+++ app.js
@@ -0,0 +1,7 @@
+import { EasyGameEngine } from './lib/EasyGameEngine/EasyGameEngine.js';
+import { PlayScene } from './lib/MyGame/Scenes/PlayScene.js';
+
+const startScene = new PlayScene();
+
+const game = new EasyGameEngine(600, 480, startScene);
+document.body.appendChild(game.canvas.element);
